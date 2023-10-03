10acfae9f2e0b2a0b084ac1cfdb9b6ef0a34193e
sinnan <sinnan.imrash@gmail.com>
2023-10-03 13:01:36 +0400

    Update next.config.js

--- src/rendering/next.config.js
+++ src/rendering/next.config.js
@@ -7,7 +7,7 @@ const publicUrl = getPublicUrl();
 
 const nextConfig = {
   // Set assetPrefix to our public URL
-  assetPrefix: publicUrl,
+  assetPrefix: 'https://cdn.mydomain.com',
 
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
